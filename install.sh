3682be870476d5fb7f446f85f79c9cc4b29dbb03
Edward Stephinson <phate345@gmail.com>
2016-07-04 21:01:45 +0100

    Fixed install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 echo "Downloading certbot.php to destination..."
-curl -o /usr/local/cwpsrv/htdocs/resources/admin/modules/certbot.php https://github.com/estephinson/certbot-cwp/blob/master/certbot.php
+curl -o /usr/local/cwpsrv/htdocs/resources/admin/modules/certbot.php https://raw.githubusercontent.com/estephinson/certbot-cwp/master/certbot.php
 echo "Adding certbot to 3rdparty.php so can be accessed from the webpanel..."
 echo '<li><a href="index.php?module=certbot"><span class="icon16 icomoon-icon-arrow-right-3"></span>Certbot</a></li>' >> /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
 echo "Certbot-module installed"
